c2cf5356848f8b4aa350baceefabe17e1e5cd570
GregV3 <grzegorz.muszynski77@gmail.com>
2023-02-08 08:58:51 +0100

    changing path for  app use

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 4002;
 
 const {Client} = require('pg'); // PostgreSQL
 
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: '1mb' }));
+
 const db = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -16,13 +20,10 @@ const db = new Client({
 });
 
 db.connect();
-// Middleware
-app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+
 // app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, "client/build")));// Recognizes Request Objects as JSON objects
-
-
+//app.use(express.static(path.join(__dirname, "client/build")));// Recognizes Request Objects as JSON objects
+app.use(express.static(path.join("client/build")));// Recognizes Request Objects as JSON objects
 
 // Allows for using information coming from forms
 app.use(express.urlencoded({ extended: true }));
