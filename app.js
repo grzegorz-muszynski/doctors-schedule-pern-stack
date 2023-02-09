cb1e79672ef6f8cdcd5befa833ea6a99b9587ffd
GregV3 <grzegorz.muszynski77@gmail.com>
2023-02-09 11:35:51 +0100

    changing express.json

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 4002;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+// app.use(express.json({ limit: '1mb' }));
+app.use(express.json());
 
 // Allows for using information coming from forms
 app.use(express.urlencoded({ extended: true }));
