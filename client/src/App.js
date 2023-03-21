3e18ff4f925dba3429b8164789e129a2a387c036
GregV3 <grzegorz.muszynski77@gmail.com>
2023-03-21 14:50:33 +0100

    Adding homepage property in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Visits } from './components/Visits';
 
 export function App() {
     return (
-        <Routes basename="/">
+        <Routes>
             <Route exact path="/" element={<Table />} />
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/visits" element={<Visits />} />
